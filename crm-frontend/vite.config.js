d8875ba4dacf004f5170f1e4043b44f8301edd63
Fernandlabarre <86474347+Fernandlabarre@users.noreply.github.com>
2025-08-08 21:27:25 +0200

    Update vite.config.js

--- crm-frontend/vite.config.js
+++ crm-frontend/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
-  server: {
-    host: '0.0.0.0',    // écoute toutes les interfaces, pas juste localhost
-    port: 5173,         // ou le port de votre choix
-  },
+  plugins: [ react() ],
 })
